488bc405d308c00201996cfce7bd0ee87e0bf533
sachushaji <sachushajiabraham@gmail.com>
2021-12-21 17:33:09 +0530

    fix issues with balance mapping

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,8 +1,10 @@
-import BN from 'bn.js'
-import { DatabaseManager, EventContext, StoreContext } from '@subsquid/hydra-common'
-import { Account, HistoricalBalance } from '../generated/model'
-import { Balances } from '../chain'
-
+import {
+  DatabaseManager,
+  EventContext,
+  StoreContext,
+} from "@subsquid/hydra-common";
+import { Account, HistoricalBalance } from "../generated/model";
+import { Balances } from "../types";
 
 export async function balancesTransfer({
   store,
@@ -10,56 +12,52 @@ export async function balancesTransfer({
   block,
   extrinsic,
 }: EventContext & StoreContext): Promise<void> {
+  const [from, to, value] = new Balances.TransferEvent(event).params;
+  const tip = extrinsic ? BigInt(extrinsic.tip.toString(10)) : BigInt(0);
 
-  const [from, to, value] = new Balances.TransferEvent(event).params
-  const tip = extrinsic ? new BN(extrinsic.tip.toString(10)) : new BN(0)
+  const fromAcc = await getOrCreate(store, Account, from.toHex());
+  fromAcc.wallet = from.toHuman();
+  fromAcc.balance = fromAcc.balance || BigInt(0);
+  fromAcc.balance = fromAcc.balance - value.toBigInt();
+  fromAcc.balance = fromAcc.balance - tip;
+  await store.save(fromAcc);
 
-  const fromAcc = await getOrCreate(store, Account, from.toHex())
-  fromAcc.wallet = from.toHuman()
-  fromAcc.balance = fromAcc.balance || new BN(0)
-  fromAcc.balance = fromAcc.balance.sub(value)
-  fromAcc.balance = fromAcc.balance.sub(tip)
-  await store.save(fromAcc)
+  const toAcc = await getOrCreate(store, Account, to.toHex());
+  toAcc.wallet = to.toHuman();
+  toAcc.balance = toAcc.balance || BigInt(0);
+  toAcc.balance = toAcc.balance + value.toBigInt();
+  await store.save(toAcc);
 
-  const toAcc = await getOrCreate(store, Account, to.toHex())
-  toAcc.wallet = to.toHuman()
-  toAcc.balance = toAcc.balance || new BN(0)
-  toAcc.balance = toAcc.balance.add(value)
-  await store.save(toAcc)
-
-  const hbFrom = new HistoricalBalance()
+  const hbFrom = new HistoricalBalance();
   hbFrom.account = fromAcc;
   hbFrom.balance = fromAcc.balance;
-  hbFrom.timestamp = new BN(block.timestamp)
-  await store.save(hbFrom)
+  hbFrom.timestamp = BigInt(block.timestamp);
+  await store.save(hbFrom);
 
-  const hbTo = new HistoricalBalance()
+  const hbTo = new HistoricalBalance();
   hbTo.account = toAcc;
   hbTo.balance = toAcc.balance;
-  hbTo.timestamp = new BN(block.timestamp)
-  await store.save(hbTo)
+  hbTo.timestamp = BigInt(block.timestamp);
+  await store.save(hbTo);
 }
 
-
-async function getOrCreate<T extends {id: string}>(
+async function getOrCreate<T extends { id: string }>(
   store: DatabaseManager,
   entityConstructor: EntityConstructor<T>,
   id: string
 ): Promise<T> {
-
   let e = await store.get(entityConstructor, {
     where: { id },
-  })
+  });
 
   if (e == null) {
-    e = new entityConstructor()
-    e.id = id
+    e = new entityConstructor();
+    e.id = id;
   }
 
-  return e
+  return e;
 }
-
 
 type EntityConstructor<T> = {
-  new (...args: any[]): T
-}
+  new (...args: any[]): T;
+};
